b57174448a16a761fd23085a19ce80137f15a237
Jorgevasco246 <1107844162@u.icesi.edu.co>
2026-09-24 08:44:57 -0500

    feat: add the columns

--- server/src/carts/entities/cart.entity.ts
+++ server/src/carts/entities/cart.entity.ts
@@ -1,1 +1,31 @@
-export class Cart {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+import { UserEntity } from '../../users/entities/user.entity';
+import { GarmentEntity } from '../../garments/entities/garment.entity';
+import { TransactionTypeEnum } from '../../transactions/entities/transaction.entity';
+
+@Entity('cart_items')
+export class CartItemEntity {
+  @PrimaryColumn({ name: 'user_id' })
+  userId!: string;
+
+  @PrimaryColumn({ name: 'garment_id' })
+  garmentId!: string;
+
+  @Column({
+    name: 'transaction_type',
+    type: 'enum',
+    enum: TransactionTypeEnum,
+  })
+  transactionType!: TransactionTypeEnum;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+}
