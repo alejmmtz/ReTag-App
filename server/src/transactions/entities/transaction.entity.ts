4499eb917ba935894c37c3852f4a50fd3d3b75a9
Jorgevasco246 <1107844162@u.icesi.edu.co>
2026-09-24 08:21:52 -0500

    feat: create the necessary enums

--- server/src/transactions/entities/transaction.entity.ts
+++ server/src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,24 @@
-export class Transaction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+import { GarmentEntity } from '../../garments/entities/garment.entity';
+
+export enum TransactionTypeEnum {
+  SALE = 'SALE',
+  TRADE = 'TRADE',
+}
+
+export enum TransactionStatusEnum {
+  PENDING = 'PENDING',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED',
+}
+
+@Entity('transactions')
+export class TransactionEntity {}
